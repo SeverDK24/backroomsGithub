2879901487a4bcf16ad8998a3171491e0150a04d
Kessler230 <polinanvua2@gmail.com>
2026-06-02 19:39:19 +0300

    Player Script
    Скріпт для плеєра

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 10f;
+    
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void PlayerMoving()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * speed);
+        }
+    }
+
+   
+}
