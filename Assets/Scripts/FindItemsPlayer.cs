6c5c186655972f0a5d7eb48322b96261c3f6ab31
Kessler230 <polinanvua2@gmail.com>
2026-08-20 18:56:12 +0300

    Lever
    дробила анімацію ричага

--- Assets/Scripts/FindItemsPlayer.cs
+++ Assets/Scripts/FindItemsPlayer.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class FindItemsPlayer : MonoBehaviour
@@ -22,5 +23,23 @@ public class FindItemsPlayer : MonoBehaviour
         }
         Debug.DrawRay(transform.position, transform.forward * distance, Color.yellow);
 
+        //Для ричага
+        if (Physics.Raycast(transform.position, transform.forward, out hit, distance))
+        {
+            if (hit.collider.CompareTag("lever"))
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Debug.Log("Було нажато");
+                    hit.collider.GetComponentInParent<Animator>().SetTrigger("Press lever");
+                }
+            }
+        }
+        
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position, transform.forward * distance);
     }
 }
