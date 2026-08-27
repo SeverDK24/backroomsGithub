3008c09d9d1cac50de7735bce03cd249080b3b76
Kessler230 <polinanvua2@gmail.com>
2026-08-27 18:48:26 +0300

    Рандомние звуки
    я добавила звуки

--- Assets/Scripts/RandomSounds.cs
+++ Assets/Scripts/RandomSounds.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class RandomSounds : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AudioSource audioSource;
+    public AudioClip[] sounds;
+
+    float timer;
+
     void Start()
     {
-        
+        timer = Random.Range(30f, 60f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            int randomSound = Random.Range(0, sounds.Length);
+            audioSource.PlayOneShot(sounds[randomSound]);
+
+            timer = Random.Range(60f, 120f);
+        }
     }
 }
